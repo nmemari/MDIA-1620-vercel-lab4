46fbb9598b5b29294b50f146389b7e575a7a1543
nmemari <work.nmemari@gmail.com>
2022-11-02 11:07:49 -0700

    files and folders

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          What does C.R.U.D. stand for?
         </p>
 
         <div className={styles.grid}>
